06adef089a9ea317e1d2dcdc5fa9906cfb9560de
JinJeongMin <jdsjm2@naver.com>
2023-11-14 00:04:24 +0900

    Feat: SearchUserBox 컴포넌트 export 제거

--- src/components/chats/SearchMyChat.tsx
+++ src/components/chats/SearchMyChat.tsx
@@ -3,52 +3,65 @@ import React, { useCallback, useState } from 'react';
 import { useRecoilState } from 'recoil';
 import styled from 'styled-components';
 import { allChatsState, myChatsState, searchChatsState } from './chatsStore';
-import { SearchButton, SearchUserBox } from '@/app/users/page';
+import { SearchButton } from '@/app/users/page';
 import { MdSearch } from 'react-icons/md';
 
 const SearchMyChat = ({ userType }: any) => {
-    const [input, setInput] = useState<string>('');
-    const [filterChats, setFilteredChats] = useRecoilState(searchChatsState);
-    const [allChats, setAllChats] = useRecoilState(allChatsState);
-    const [myChats, setAllMyChats] = useRecoilState(myChatsState);
-    const onInputChange = useCallback(
-        (e: React.ChangeEvent<HTMLInputElement>) => {
-            setInput(e.target.value);
-            console.log(myChats);
-            const filteringChats = (userType == 'my' ? myChats : allChats).filter((chat) => {
-                const includesValue = chat.name.toLowerCase().startsWith(e.target.value.toLowerCase());
-                return includesValue;
-            });
-            console.log('Filtering Chats:', filteringChats);
-            const filteredChats = [...filteringChats];
-            console.log('Filtered Chats:', filteredChats);
-            setFilteredChats(filteredChats);
-        },
-        [myChats, allChats],
-    );
-    return (
-        <Wrapper>
-            <SearchUserBox>
-                <SearchButton>
-                    <MdSearch className="searchIcon" size="35" color="white" />
-                </SearchButton>
-                <SearchItem type="text" placeholder="검색" onChange={onInputChange} />
-            </SearchUserBox>
-        </Wrapper>
-    );
+  const [input, setInput] = useState<string>('');
+  const [filterChats, setFilteredChats] = useRecoilState(searchChatsState);
+  const [allChats, setAllChats] = useRecoilState(allChatsState);
+  const [myChats, setAllMyChats] = useRecoilState(myChatsState);
+  const onInputChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setInput(e.target.value);
+      console.log(myChats);
+      const filteringChats = (userType == 'my' ? myChats : allChats).filter((chat) => {
+        const includesValue = chat.name.toLowerCase().startsWith(e.target.value.toLowerCase());
+        return includesValue;
+      });
+      console.log('Filtering Chats:', filteringChats);
+      const filteredChats = [...filteringChats];
+      console.log('Filtered Chats:', filteredChats);
+      setFilteredChats(filteredChats);
+    },
+    [myChats, allChats],
+  );
+  return (
+    <Wrapper>
+      <SearchUserBox>
+        <SearchButton>
+          <MdSearch className="searchIcon" size="35" color="white" />
+        </SearchButton>
+        <SearchItem type="text" placeholder="검색" onChange={onInputChange} />
+      </SearchUserBox>
+    </Wrapper>
+  );
 };
 
 export default SearchMyChat;
 
 const Wrapper = styled.div`
-    width: 100%;
-    display: flex;
-    justify-content: center;
+  width: 100%;
+  display: flex;
+  justify-content: center;
 `;
 
 const SearchItem = styled.input`
-    border: none;
-    width: 32rem;
-    outline: none;
-    font-size: 1.2rem;
+  border: none;
+  width: 32rem;
+  outline: none;
+  font-size: 1.2rem;
+`;
+
+const SearchUserBox = styled.div`
+  background-color: white;
+
+  border-radius: 20px;
+  box-shadow: ${({ theme }) => theme.shadow.search};
+
+  width: 100%;
+  height: 3.5rem;
+
+  display: flex;
+  gap: 3%;
 `;
