899e62f5f0b526aaa453340a37c9cf38395ddb5b
JinJeongMin <jdsjm2@naver.com>
2023-11-14 10:39:31 +0900

    Feat: 특정 ChatId API 적용

--- src/app/chating/[route]/page.tsx
+++ src/app/chating/[route]/page.tsx
@@ -1,9 +1,10 @@
+import React from 'react';
 import ChatingPage from '../../../components/chating/ChatingPage';
 
 export default function Chating() {
-    return (
-        <main>
-            <ChatingPage />
-        </main>
-    );
+  return (
+    <main>
+      <ChatingPage />
+    </main>
+  );
 }
